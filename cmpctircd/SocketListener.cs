f774a0353623e6f9da3922f6aa3ef02386ae7680
Calvin <calvin@openmailbox.org>
2017-01-10 15:30:23 -0400

    Use modern async, also raise questions for sam

--- cmpctircd/SocketListener.cs
+++ cmpctircd/SocketListener.cs
@@ -9,12 +9,13 @@ using System.Net.Sockets;
 using System.Collections;
 
 namespace cmpctircd {
-	class SocketListener {
+    class SocketListener {
         private Boolean _started = false;
         private TcpListener _listener = null;
         // port
         //private Dictionary<TcpClient, Client> client_mapping = new Dictionary<Socket, Client>();
-        private ArrayList _sockets = new ArrayList();
+        // maybe this should be tcpclients
+        private List<Socket> _sockets = new List<Socket>();
 
         public SocketListener(String IP, int port) {
             _listener = new TcpListener(IPAddress.Any, port);
@@ -35,13 +36,50 @@ namespace cmpctircd {
 
         // Accept + read from clients.
         // This function should be called in a loop
-        public void listenToClients() {
+        public async void listenToClients() {
             if (!_started) {
                 throw new InvalidOperationException("Bind() not called or has been stopped.");
             }
 
-            _listener.BeginAcceptTcpClient(new AsyncCallback(acceptClient), _listener);
+            // TODO: Loop (should be done by caller instead)
+            TcpClient tc = await _listener.AcceptTcpClientAsync();
+            HandleClient(tc);
         }
+
+        async Task HandleClient(TcpClient tc) {
+            var client = new Client();
+            client.socket = tc.Client;
+            client.buffer = new byte[1024];
+
+            lock (_sockets)
+                _sockets.Add(client.socket);
+
+            // use Stream - socket draft shown here
+            //var rEvent = new SocketAsyncEventArgs();
+            //rEvent.SetBuffer(client.buffer, 0, client.buffer.Length);
+            //tc.Client.ReceiveAsync(rEvent);
+
+            // TODO: Loops
+
+            using (var s = tc.GetStream()) {
+                var bytesRead = await s.ReadAsync(client.buffer, 0, client.buffer.Length);
+                if (bytesRead > 0) {
+                    // Would a TcpClient have ReadLine for us?
+                    string data = Encoding.UTF8.GetString(client.buffer);
+                    Console.WriteLine("Got data:");
+                    Console.WriteLine(data);
+                } else {
+                    Console.WriteLine("No data, killing client");
+                    // Close the connectiono
+                    client.socket.Close();
+                    lock (_sockets) {
+                        _sockets.Remove(client.socket);
+                    }
+                }
+            }
+        }
+
+        // dead code starts here
 
         public void acceptClient(IAsyncResult ar) {
             TcpListener listener = (TcpListener)ar.AsyncState;
@@ -54,10 +92,11 @@ namespace cmpctircd {
 
             Console.WriteLine("Accepting a client");
             lock (_sockets) {
-            	_sockets.Add(client.socket);
+                _sockets.Add(client.socket);
             }
 
             client.socket.BeginReceive(client.buffer, 0, client.buffer.Length, SocketFlags.None, new AsyncCallback(readClient), client);
+            // shouldn't the caller be the one looping?
             _listener.BeginAcceptTcpClient(new AsyncCallback(acceptClient), _listener);
         }
 

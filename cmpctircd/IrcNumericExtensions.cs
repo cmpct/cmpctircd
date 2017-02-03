8cb60f109700c511483bcb3fba41eafa1f9e142f
Calvin <calvin@openmailbox.org>
2017-02-03 17:41:56 -0400

    IRC numeric extension to print w/ leading 0s

---
+++ cmpctircd/IrcNumericExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cmpctircd
+{
+    public static class IrcNumericExtensions
+    {
+        /// <summary>
+        /// Formats an IRC numeric response as a string, complete with leading
+        /// zeros.
+        /// </summary>
+        /// <param name="n">The IRC numeric to format.</param>
+        /// <returns>The formatted numeric.</returns>
+        public static string Printable(this IrcNumeric n)
+        {
+            return string.Format("{0:000}", (int)n);
+        }
+    }
+}

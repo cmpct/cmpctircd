0644f2bd4fd119cd3ac8d9ff4ddecfe696640b06
sam <sam@cmpct.info>
2017-07-28 09:48:06 +0100

    Tolerate 'PONG cookie'
    https://bugs.cmpct.info/show_bug.cgi?id=201

--- cmpctircd/Packets/Login.cs
+++ cmpctircd/Packets/Login.cs
@@ -70,6 +70,13 @@ namespace cmpctircd.Packets {
             String[] splitLine = rawLine.Split(new char[] { ':' }, 2);
             String cookie = splitLine[1];
 
+            // We've assumed the message is: PONG :cookie (or PONG server :cookie)
+            // But some clients seem to send PONG cookie, so look for that if we've not found a cookie
+            if(String.IsNullOrEmpty(cookie)) {
+                splitLine = rawLine.Split(new char[] { ' '}, 2);
+                cookie    = splitLine[1];
+            }
+
             if(client.PingCookie == cookie) {
                 client.WaitingForPong = false;
                 client.LastPong = (Int32)(DateTime.Now.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;

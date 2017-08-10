8140331e47e2c5bdfc56cd3ca0e7278ea8a2c155
sam <sam@cmpct.info>
2017-08-10 14:47:02 +0100

    Make test paths dynamic
    Don't hardcode paths to the SmartIrc4net, NUnit assemblies. The fewer hardcoded paths the better.

--- cmpctircd/Tests/build.sh
+++ cmpctircd/Tests/build.sh
@@ -1,4 +1,14 @@
+## config
+TEST_FILES="ChannelInOutTest.cs"
+# path to dependencies
+NUNIR_PATH="bin/net-4.5/nunitlite-runner.exe"
+NUNIT_PATH="bin/net-4.5/nunit.framework.dll"
+SMART_PATH="bin/Meebey.SmartIrc4net.dll"
+# where to create test assembly
+TEST_PATH="Tests.dll"
+## end config
+
 # build
-csc -t:library -r:bin/net-4.5/nunit.framework.dll -r:bin/Meebey.SmartIrc4net.dll ChannelInOutTest.cs
+csc /out:${TEST_PATH} -t:library -r:${NUNIT_PATH} -r:${SMART_PATH} ${TEST_FILES}
 # run
 mono bin/net-4.5/nunitlite-runner.exe Tests.dll

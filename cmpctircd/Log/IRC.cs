b32e632e026eb794230627ae355d1c25e1963f90
sam <sam@cmpct.info>
2017-09-01 20:50:06 +0100

    Allow logging channels with default modes
    Details in bug 218. Will be more useful when oper-only mode (+O) is
    implemented (bug 220), itself depending on ircops (bug 219).
    
    https://bugs.cmpct.info/show_bug.cgi?id=218

--- cmpctircd/Log/IRC.cs
+++ cmpctircd/Log/IRC.cs
@@ -6,6 +6,7 @@ namespace cmpctircd {
     public class IRC : BaseLogger {
 
         private Channel _Channel;
+        private Client _PsuedoClient;
 
         public IRC(IRCd ircd, LogType type) : base(ircd, type) {}
 
@@ -24,6 +25,48 @@ namespace cmpctircd {
                 _Channel = IRCd.ChannelManager.Create(channelName);
             }
 
+            // Create a psuedo client with which to send messages
+            _PsuedoClient = new Client(IRCd, new System.Net.Sockets.TcpClient(), null);
+            _PsuedoClient.Nick = IRCd.Host;
+
+            // Apply any modes - if we have them
+            if(arguments.ContainsKey("modes")) {
+                // We expect the modes in the format "+<modes>" (e.g. modes="+nt")
+                // (or "-<modes>" but that's not likely)
+                // TODO: Allow modes with arguments (e.g. +k; only becomes pressing once that is implemented)
+                var modes     = arguments["modes"];
+                var modifiers = new List<string>() { "+", "-" };
+                var modifier  = "";
+                foreach (var modeCharacter in modes.ToCharArray()) {
+                    if (modifiers.Contains(modeCharacter.ToString())) {
+                        modifier = modeCharacter.ToString();
+                        continue;
+                    }
+
+                    var mode = false;
+                    try {
+                        switch (modifier) {
+                            case "+":
+                                mode = _Channel.Modes[modeCharacter.ToString()].Grant(_PsuedoClient, null, true, true, false);
+                                break;
+
+                            case "-":
+                                mode = _Channel.Modes[modeCharacter.ToString()].Revoke(_PsuedoClient, null, true, true, false);
+                                break;
+
+                            default:
+                                IRCd.Log.Warn("No mode prefix +/- in logger IRC settings; please add one xor the other (e.g. +nt)");
+                                break;
+                        }
+                    } catch (Exception) {}
+
+                    if(!mode) {
+                        IRCd.Log.Error($"Could not set mode {modeCharacter.ToString()} on {_Channel.Name} for logging!");
+                        IRCd.Log.Error($"Please review your log config (modes) for the IRC logger because of this failure.");
+                    }
+                }
+            }
+
             _Channel.CanDestroy = false;
         }
 
@@ -38,7 +81,7 @@ namespace cmpctircd {
 
         override public void WriteLine(string msg, LogType type, bool prepared = true) {
             if(!prepared) msg = Prepare(msg, type);
-            _Channel.SendToRoom(null, msg);
+            _Channel.SendToRoom(_PsuedoClient, msg);
         }
 
 

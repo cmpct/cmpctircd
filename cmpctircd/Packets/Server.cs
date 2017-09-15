6603b905cd75b553b614f13ea4dea0f1f8f7ef94
sam <sam@cmpct.info>
2017-09-15 18:42:58 +0100

    Commit Server class

---
+++ cmpctircd/Packets/Server.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cmpctircd.Packets {
+    class Server {
+
+        public Server(IRCd ircd) {
+            /*ircd.PacketManager.Register(new PacketManager.HandlerInfo() {
+                Packet = "HELLO",
+                Handler = Test,
+                Type = ListenerType.Server
+            });*/
+        }
+
+        public bool Test(HandlerArgs args) {
+            args.Sender.Write("hello");
+            return true;
+        }
+    }
+}

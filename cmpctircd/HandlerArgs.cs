7fc4c013e78141cb254c512336ec10908503ee81
sam <sam@cmpct.info>
2018-06-18 21:36:25 +0100

    Make HandlerArgs pre-process arguments
    This will allow (repetitive) logic/handling to be moved out of the
    individual handlers. See bug. Individual handlers will need to move to
    the new method.
    
    http://bugs.cmpct.info/show_bug.cgi?id=255

--- cmpctircd/HandlerArgs.cs
+++ cmpctircd/HandlerArgs.cs
@@ -17,18 +17,46 @@ namespace cmpctircd
         public string Line { get; set; }
         public bool Force { get; set; }
 
-        public HandlerArgs(IRCd ircd, Client client, string line, bool force) {
+        public List<string> SpacedArgs;
+        public string       Trailer;
+
+        public HandlerArgs(IRCd ircd, string line, bool force) {
             IRCd = ircd;
-            Client = client;
             Line = line;
             Force = force;
+
+            // Split the arguments first by space (we use this a few times)
+            SpacedArgs     = Line.Split(new char[] {' '}).ToList();
+
+            // Trailing arguments
+            // Denoted by a :, usually one continuous argument
+            // Only interested in arguments not in the first chunk
+            List<string> ModifiedLine = SpacedArgs;
+            string       ModifiedLineJoined;
+
+            ModifiedLine.RemoveAt(0);
+            ModifiedLineJoined = String.Join(" ", ModifiedLine);
+            Trailer            = ModifiedLineJoined.Contains(":") ? ModifiedLineJoined.Split(new char[] {':'})[1] : null;
+
+            // Spaced arguments
+            // Make a list of the spaced arguments up until we see a colon (:)
+            // e.g. LIST #channel -> {"LIST", "#channel"}
+
+            // Check if there's :(U)UID in there, if so, strip it out
+            // Those who need it can grab it from the raw Line
+            if(SpacedArgs[0].Contains(":")) {
+                SpacedArgs.RemoveAt(0);
+            }
+
+            SpacedArgs = SpacedArgs.TakeWhile(arg => !arg.Contains(":")).ToList<string>();
         }
 
-        public HandlerArgs(IRCd ircd, Server server, string line, bool force) {
-            IRCd = ircd;
+        public HandlerArgs(IRCd ircd, Client client, string line, bool force) : this(ircd, line, force) {
+            Client = client;
+        }
+
+        public HandlerArgs(IRCd ircd, Server server, string line, bool force) : this(ircd, line, force) {
             Server = server;
-            Line = line;
-            Force = force;
         }
 
     }

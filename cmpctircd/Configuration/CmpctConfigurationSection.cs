376eeed89b8560d6f2d2c81d47abedd63294fd33
Lewis Hazell <lewis.hazell@hotmail.co.uk>
2019-06-11 14:10:30 +0100

    Copied ircd.xml to App.config (with modifications) Created CmpctConfigurationSection class

---
+++ cmpctircd/Configuration/CmpctConfigurationSection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace cmpctircd.Configuration {
+    public class CmpctConfigurationSection : ConfigurationSection {
+        public static CmpctConfigurationSection GetConfiguration() {
+            return (CmpctConfigurationSection) ConfigurationManager.GetSection("cmpct") ?? new CmpctConfigurationSection();
+        }
+    }
+}

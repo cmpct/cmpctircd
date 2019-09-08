555b38a5f6dca195ac093ef518eb878a218c466a
sam <sam@cmpct.info>
2019-09-08 23:59:12 +0100

    IRCD-14 - Consoldiate common server messages
    - Move PING, PONG to Server/Inbound/Any so that the implementation is the same
      for all IRCds
    
    - Add ERROR with logging and a disconnect if sent (this is always a critical
      error when sent by a server)

--- cmpctircd/Server/Inbound/InspIRCd21.cs
+++ cmpctircd/Server/Inbound/InspIRCd21.cs
@@ -10,28 +10,6 @@ namespace cmpctircd.Packets {
         // TODO: three CAPAB packets
         // TODO: Handle BURST, don't process until we get them all? Group the FJOINs
 
-        [Handler("PING", ListenerType.Server, ServerType.InspIRCd21)]
-        public static bool PingHandler(HandlerArgs args) {
-            // TODO: implement for hops > 1
-            // TODO: could use args.Server.SID instead of SpacedArgs?
-            var pingCookie = "";
-            if (args.SpacedArgs.Count == 1) {
-                pingCookie = args.SpacedArgs[0];
-            } else {
-                pingCookie = args.SpacedArgs[1];
-            }
-
-            args.Server.Write($":{args.IRCd.SID} PONG {args.IRCd.SID} {pingCookie}");
-            return true;
-        }
-
-        [Handler("PONG", ListenerType.Server, ServerType.InspIRCd21)]
-        public static bool PongHandler(HandlerArgs args) {
-            args.Server.WaitingForPong = false;
-            args.Server.LastPong       = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-            return true;
-        }
-
         [Handler("CAPAB", ListenerType.Server, ServerType.InspIRCd21)]
         public static bool CapabHandler(HandlerArgs args) {
             // TODO: checked if already sent capab?

2b959ebd78bb286381ce016677b4a99e4c20f792
joshoxe <joshoxenham1@gmail.com>
2020-10-24 16:03:13 +0100

    IRCD-51 Remove unused imports

--- cmpctircd/Cloak/Service/InspIRCd/InspDNSService.cs
+++ cmpctircd/Cloak/Service/InspIRCd/InspDNSService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace cmpctircd.Cloak.Service
+﻿namespace cmpctircd.Cloak.Service
 {
     public class InspDnsService : CloakService
     {

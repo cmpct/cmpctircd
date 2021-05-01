4937b7939291cd35f77043cbf5765f1f14bf9256
Lewis Hazell <lewis.hazell@hotmail.co.uk>
2021-05-01 18:23:13 +0200

    IRCD-54 Add controllers as transient services.
    Add IrcContext and Client/Server as scoped services.

--- cmpctircd/Program.cs
+++ cmpctircd/Program.cs
@@ -1,5 +1,8 @@
 ﻿namespace cmpctircd {
+    using System;
+    using System.Linq;
     using cmpctircd.Configuration;
+    using cmpctircd.Controllers;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
 
@@ -14,9 +17,17 @@
                 .ConfigureServices((hostContext, services) => {
                     var log = new Log();
                     var config = CmpctConfigurationSection.GetConfiguration();
+
+                    foreach (var controllerType in AppDomain.CurrentDomain.GetAssemblies().SelectMany(t => t.GetTypes()).Where(t => typeof(ControllerBase).IsAssignableFrom(t))) {
+                        services.AddTransient(controllerType);
+                    }
+
                     services.AddSingleton(config);
                     services.AddSingleton(log);
                     services.AddSingleton<IRCd>();
+                    services.AddScoped<IrcContext>();
+                    services.AddScoped(sp => sp.GetRequiredService<IrcContext>().Sender as Client);
+                    services.AddScoped(sp => sp.GetRequiredService<IrcContext>().Sender as Server);
                     services.AddHostedService<IrcApplicationLifecycle>();
                 });
         }

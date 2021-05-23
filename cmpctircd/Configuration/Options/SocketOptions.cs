b6a55be0d9001f48264265361253af27758b18cf
Josh Oxenham <18144362+joshoxe@users.noreply.github.com>
2021-05-23 15:59:02 +0100

    Configuration: Clean up SocketOptions
    task: #9

--- cmpctircd/Configuration/Options/SocketOptions.cs
+++ cmpctircd/Configuration/Options/SocketOptions.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace cmpctircd.Configuration
-{
-    public class SocketOptions
-    {
+﻿namespace cmpctircd.Configuration {
+    public class SocketOptions {
         public SocketElement[] Sockets { get; set; }
     }
 }
